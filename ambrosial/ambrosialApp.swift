c448e39e5f418f08533fd06e13fb4150af3755a5
yuhongz12 <yuhong.zhao12@gmail.com>
2021-10-24 12:29:05 -0400

    initial commit

---
+++ ambrosial/ambrosialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ambrosialApp.swift
+//  ambrosial
+//
+//  Created by yuhongMac on 10/21/21.
+//
+
+import SwiftUI
+
+@main
+struct ambrosialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
