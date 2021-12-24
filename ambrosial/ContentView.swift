f303b5e2424b9fbcbecae884dda3d5fa6a6edc30
yuhongz12 <yuhong.zhao12@gmail.com>
2021-12-24 12:50:13 -0500

    remove initalized comment

--- ambrosial/ContentView.swift
+++ ambrosial/ContentView.swift
@@ -2,8 +2,7 @@
 //  ContentView.swift
 //  ambrosial
 //
-//  Created by yuhongMac on 10/21/21.
-//
+
 
 import SwiftUI
 
@@ -75,7 +74,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-
-// this is a line of code
-// hellow world
